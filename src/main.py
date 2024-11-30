a80ba0cc81d912cbc46437944f8dc2f809e1bd61
Феодор Калашов <feodorkalasov@MacBook-Air-Feodor.local>
2024-11-30 22:01:13 +0300

    Add cmd args pars

--- src/main.py
+++ src/main.py
@@ -1,13 +1,31 @@
+import argparse
+
 from NetworkDeviceParser import NetworkDeviceParser
 from Serializer import Serializer
 
 
 if __name__ == '__main__':
-    parser = NetworkDeviceParser()
+    parser = argparse.ArgumentParser(description="Парсер и сериализатор сетевых устройств.")
+    parser.add_argument(
+        '-i', '--input_file',
+        required=True,
+        help="Путь к входному файлу с данными сетевых устройств."
+    )
+    parser.add_argument(
+        '-o', '--output_file',
+        required=True,
+        help="Путь к выходному файлу, куда будет записан результат в формате XML."
+    )
+    args = parser.parse_args()
+
+    output_file_path: str = args.input_file
+    result_xml_file_path: str = args.output_file
+
+    device_parser = NetworkDeviceParser()
     serializer = Serializer()
 
-    network_devices_dict = parser.get_network_devices_settings_from_file(
-        net_dev_output_file_path='./network_device_output.txt'
+    network_devices_dict = device_parser.get_network_devices_settings_from_file(
+        net_dev_output_file_path=output_file_path
     )
 
     xml_string = serializer.serialize(
@@ -16,5 +34,6 @@ if __name__ == '__main__':
 
     formatted_xml_strings = serializer.prettify_xml_string(
         xml_string=xml_string,
-        with_recording_to_file=True
+        with_recording_to_file=True,
+        file_path=result_xml_file_path
     )
